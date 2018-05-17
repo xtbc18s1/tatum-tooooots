02e25c42fca3072738f77e4e160e2b71445550de
Dave Strus <dave@getfretless.com>
2018-05-17 11:15:00 -0400

    Add the flick to an unordered list

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const app = {
-  init: function(formSelector) {
+  init: function(selectors) {
     this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
 
     document
-      .querySelector(formSelector)
+      .querySelector(selectors.formSelector)
       .addEventListener('submit', ev => {
         ev.preventDefault()
         this.handleSubmit(ev)
       })
+  },
+
+  renderListItem: function(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
   },
 
   handleSubmit: function(ev) {
@@ -16,9 +23,15 @@ const app = {
       id: ++this.max,
       name: f.flickName.value,
     }
-    console.log(flick)
+
+    const item = this.renderListItem(flick)
+    this.list.appendChild(item)
+
     f.reset()
   },
 }
 
-app.init('#flickForm')
+app.init({
+  formSelector: '#flickForm',
+  listSelector: '#flickList',
+})
